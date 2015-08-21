a81ce24c445409a9989f3b5b71e92f1f1bf2deb5
donskifarrell <donskifarrell@googlemail.com>
2015-08-21 14:21:39 +0100

    Fixed Disqus comments boxes loading correctly - need to test

--- javascripts/disqus.js
+++ javascripts/disqus.js
@@ -2,15 +2,34 @@ var disqus_shortname = 'donalfarrellblog';
 var disqus_identifier;
 var disqus_url;
 
+var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js'; // Found in disqus.js script
+$('head').append(dsq);
+
 function loadDisqus(element, postTitle, postUrlTag) {
   var identifier = postTitle;
   var url = window.location.origin + '/#!' + postUrlTag;
 
-  if (window.DISQUS) {
-    $(".disqus_thread").remove();
-    $(element).append('<div id="disqus_thread"></div>');
-    /** if Disqus exists, call it's reset method with new parameters **/
+  var disqus_identifier = identifier;
+  var disqus_url = url;
 
+  if (window.DISQUS) {
+    // Horrible, but jQuery wasn't removing the div elements
+    $( ".comments-load" ).each(function(){
+      var len = this.childNodes.length;
+
+      for(var i = 0; i < len; i++)
+      {  
+        if (this.childNodes[i].tagName == "DIV"){
+          this.removeChild(this.childNodes[i]);
+        } 
+      }
+
+    });
+
+    $(element).append('<div class="disqus-thread" id="disqus_thread"></div>');
+
+    /** if Disqus exists, call it's reset method with new parameters **/
     DISQUS.reset({
       reload: true,
       config: function () { 
@@ -18,16 +37,5 @@ function loadDisqus(element, postTitle, postUrlTag) {
         this.page.url = url;
       }
     });
-  }
-  else {
-    //insert a wrapper in HTML after the relevant "show comments" link
-    jQuery('<div id="disqus_thread"></div>').insertAfter(element);
-    disqus_identifier = identifier; //set the identifier argument
-    disqus_url = url; //set the permalink argument
-
-    //append the Disqus embed script to HTML
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    jQuery('head').append(dsq);
   }
 };
